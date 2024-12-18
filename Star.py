6525b1cc0a5820c60f67b6f3455bf561ce42e9a4
DestructiveSillyonker <144646094+DestructiveSillyonker@users.noreply.github.com>
2024-12-18 11:44:14 -0800

    1

--- Star.py
+++ Star.py
@@ -1,12 +1,37 @@
 import random
 
 
-def StarSystem():
-    output = {}
+
+def starport():
+
+    sp = random.randint(2,12)
+    if 2 <= sp <= 4:
+       22
+    if 5 <= sp <= 6:
+       22
+    if 7 <= sp <= 8:
+       22
+    if 2 <= sp <= 4:
+       22
+    if 2 <= sp <= 4:
+       22
+       
 
 
 
-    Sports = {}
+
+
+
+
+
+
+
+#def StarSystem():
+
+starport()
+
+
+    
 
 
 
